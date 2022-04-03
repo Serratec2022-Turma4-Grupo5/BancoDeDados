4300f4a6179bab92babb212e0af69b86457f5499
RosanaDantas <rfdrosana@gmail.com>
2022-04-03 16:36:54 -0300

    criação do banco e inserção de algumas infrmações(categoria, funcionário,produto)

---
+++ e-commerce/ecommerce_schema.sql
@@ -0,0 +1,21 @@
+INSERT INTO "Trabalho_Final".produto
+(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Shampoo Argan', 32, '2022-01-25', 48.99, '2022-01-30', 1, 1);
+
+INSERT INTO "Trabalho_Final".produto
+(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Condicionador Argan', 32, '2022-01-25', 28.50, '2022-01-30', 1, 1);
+
+INSERT INTO "Trabalho_Final".produto
+(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Máscara Hidratação', 123, '2022-01-25', 62.99, '2022-01-30', 1, 1);
+
+INSERT INTO "Trabalho_Final".produto
+(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Creme de pentear', 333, '2022-01-25', 20.90, '2022-01-30', 1, 1);
+
+INSERT INTO "Trabalho_Final".produto
+(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Óleo Capilar Argan', 200, '2022-01-25', 34.90, '2022-01-30', 1, 1);
+
+select * from produto
