6fa4f9e0ba184df38e14a03f9dac1824958c4586
RosanaDantas <rfdrosana@gmail.com>
2022-04-04 19:16:59 -0300

    Ajuste do insert da tabela Cliente, FK da tabela Pedido

--- e-commerce/Script-6.sql
+++ e-commerce/Script-6.sql
@@ -1,3 +1,6 @@
+Rodar na sequência
+
+
 CREATE TABLE "funcionario" (
   "codigo_func" serial Not Null,
   "nome_func" varchar(50) Not Null ,
@@ -37,6 +40,14 @@ CREATE TABLE "endereco" (
   
 );
 
+CREATE TABLE "contato"(
+  "codigo_contato" serial Not Null,
+  "telefone_fixo" char(10),
+  "telefone_celular"char(11),
+  "email"varchar(30),
+  PRIMARY KEY ("codigo_contato")
+);
+
 CREATE TABLE "pedido" (
   "codigo_compra" serial Not Null,
   "data_compra" timestamp,
@@ -49,12 +60,6 @@ CREATE TABLE "pedido" (
       REFERENCES "cliente"("codigo_cliente")
 );
 
-
-
-CREATE TABLE "contato"(
-  "codigo_contato" serial Not Null,
-  PRIMARY KEY ("codigo_contato")
-);
 
 CREATE TABLE "produto" (
   "codigo_produto" serial Not Null,
@@ -74,67 +79,98 @@ CREATE TABLE "produto" (
       REFERENCES "funcionario"("codigo_func")
 );
 
+--------------------------------------------------------------------------------------------
+
 alter table cliente add CONSTRAINT "FK_cliente.codigo_endereco" FOREIGN KEY ("codigo_endereco")
  REFERENCES "endereco"("codigo_endereco"); 
 
-alter table cliente add CONSTRAINT "FK_cliente.codigo_contato" FOREIGN KEY ("codigo_endereco")
+alter table cliente add CONSTRAINT "FK_cliente.codigo_contato" FOREIGN KEY ("codigo_contato")
  REFERENCES "contato"("codigo_contato");
 
+alter table pedido add CONSTRAINT "FK_pedido.codigo_produto" FOREIGN KEY ("codigo_produto")
+ REFERENCES "produto"("codigo_produto");
 
+--------------------------------------------------------------------------------------------
 
-
-INSERT INTO "Trabalho_Final".cliente
-(cpf_cliente, nomecompleto_cliente, login_cliente, senha_cliente, codigo_contato, data_nasc_cliente, codigo_endereco, data_de_cadastro)
-VALUES('55937547055', 'Luiz Carlos Martins', 'martins', 'mar123', 1, '1985-05-03', 1, '2022-03-03 00:00:00.000');
-
-
-
-INSERT INTO "Trabalho_Final".endereco
-(logradouro, complemento, bairro, cidade, "UF", cep)
-VALUES('Rua Melo Franco', '200', 'Alto', 'Teresópolis', 'RJ', 25960530);
-
-
-
-
-INSERT INTO "Trabalho_Final".funcionario
-(nome_func, cpf_func, salario) VALUES 
+INSERT INTO funcionario(nome_func, cpf_func, salario) VALUES 
 ('João Gomes', '14432253045', 2500),
 ('Larissa Manoela', '17591845004', 2500);
 
-INSERT INTO "Trabalho_Final".contato
-(telefone_fixo, telefone_celular, email)
+
+INSERT INTO endereco(logradouro, complemento, bairro, cidade, "UF", cep)VALUES
+('Rua Melo Franco', '200', 'Alto', 'Teresópolis', 'RJ', 25960530),
+('Rua Braga', '210', 'Varzea', 'Teresópolis', 'RJ', 25960630);
+
+
+
+INSERT INTO contato(telefone_fixo, telefone_celular, email)
 VALUES('2126425875', '21999826532', 'luiz@gmail.com');
 
+INSERT INTO cliente(cpf_cliente, nomecompleto_cliente, login_cliente, senha_cliente, codigo_contato, data_nasc_cliente, codigo_endereco, data_de_cadastro)
+VALUES('55937547055', 'Luiz Carlos Martins', 'martins', 'mar123', 1, '1985-05-03', 1, '2022-03-03');
 
-
-
-INSERT INTO "Trabalho_Final".produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
-	 ('Shampoo Argan',32,'2022-01-25',48.99,'2022-01-30 00:00:00',1,1),
-	 ('Condicionador Argan',32,'2022-01-25',28.50,'2022-01-30 00:00:00',1,1),
-	 ('Máscara Hidratação',123,'2022-01-25',62.99,'2022-01-30 00:00:00',1,1),
-	 ('Creme de pentear',333,'2022-01-25',20.90,'2022-01-30 00:00:00',1,1),
-	 ('Óleo Capilar Argan',200,'2022-01-25',34.90,'2022-01-30 00:00:00',1,1);
-
-
-INSERT INTO "Trabalho_Final".produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
-	 ('Sabonete Líquido Mediterranêo',176,'2021-12-05',25.99,'2022-03-01 00:00:00',1,2),
-	 ('Sabonete Barra Amazônia ',198,'2021-12-15',6.99,'2022-01-25 00:00:00',1,2),
-	 ('Antitranspirante Aloe Vera',198,'2021-12-16',20.99,'2022-03-01 00:00:00',2,2),
-	 ('Hidratante Corporal Caribe',244,'2021-12-17',56.90,'2022-03-06 00:00:00',2,2),
-	 ('Body Splash Lavanda',211,'2021-11-25',75.00,'2022-02-15 00:00:00',2,2);
-
-
-INSERT INTO "Trabalho_Final".produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
-	 ('Lapis de Olho',213,'2021-12-05',33.99,'2022-03-01 00:00:00',1,3),
-	 ('Rímel Sensacional',231,'2021-12-16',49.99,'2022-03-01 00:00:00',2,3),
-	 ('Pó Ilumidador Ilumina Geral',166,'2021-12-15',39.99,'2022-01-25 00:00:00',1,3),
-	 ('Paleta de sombra Tons pastéis',172,'2021-12-17',99.99,'2022-03-06 00:00:00',2,3),
-	 ('Baton Líquido Nude',135,'2021-11-25',54.90,'2022-02-15 00:00:00',2,3);
+----------------------------------------------------------------------------------------------------
+INSERT INTO categoria(nome_categoria)values
+('Cabelo'),
+('Perfumaria'),
+('Maquiagem');
 
 
 
 
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Shampoo Argan',32,'2022-01-25',48.99,'2022-01-30 00:00:00',1,1);
 
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Shampoo Argan',32,'2022-01-25',48.99,'2022-01-30 00:00:00',1,1);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Máscara Hidratação',123,'2022-01-25',62.99,'2022-01-30 00:00:00',1,1);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Creme de pentear',333,'2022-01-25',20.90,'2022-01-30 00:00:00',1,1);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)
+VALUES('Óleo Capilar Argan',200,'2022-01-25',34.90,'2022-01-30 00:00:00',1,1);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)values
+('Sabonete Líquido Mediterranêo',176,'2021-12-05',25.99,'2022-03-01 00:00:00',1,2);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)values
+('Sabonete Barra Amazônia ',198,'2021-12-15',6.99,'2022-01-25 00:00:00',1,2);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)values
+('Antitranspirante Aloe Vera',198,'2021-12-16',20.99,'2022-03-01 00:00:00',2,2);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)values
+('Hidratante Corporal Caribe',244,'2021-12-17',56.90,'2022-03-06 00:00:00',2,2);
+
+INSERT INTO produto(nome_produto, qtd_estoque, data_fabricacao, vl_unitario, data_cadastro, codigo_func, codigo_categoria)values
+('Body Splash Lavanda',211,'2021-11-25',75.00,'2022-02-15 00:00:00',2,2);
+
+INSERT INTO produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
+ ('Lapis de Olho',213,'2021-12-05',33.99,'2022-03-01 00:00:00',1,3);
+
+INSERT INTO produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
+ ('Rímel Sensacional',231,'2021-12-16',49.99,'2022-03-01 00:00:00',2,3);
 	
+INSERT INTO produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
+ ('Pó Ilumidador Ilumina Geral',166,'2021-12-15',39.99,'2022-01-25 00:00:00',1,3);
+	
+INSERT INTO produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
+ ('Paleta de sombra Tons pastéis',172,'2021-12-17',99.99,'2022-03-06 00:00:00',2,3);
+	
+INSERT INTO produto (nome_produto,qtd_estoque,data_fabricacao,vl_unitario,data_cadastro,codigo_func,codigo_categoria) VALUES
+ ('Baton Líquido Nude',135,'2021-11-25',54.90,'2022-02-15 00:00:00',2,3);
+	
+
+
+
+
+
+
+
+
+
 
 
